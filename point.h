78ec18fec9cb7fc48ca71c122c09a9f10973aa12
Steven Schronk <steven@schronk.com>
2014-04-23 11:07:44 -0500

    Added point and polygon primitives.

---
+++ point.h
@@ -0,0 +1,24 @@
+#ifndef POINT_T
+#define POINT_T
+
+class point_t {
+public:
+	point_t();
+	point_t(double, double);
+	point_t(const point_t&);
+
+	point_t operator + (const point_t&) const;
+	point_t operator - (const point_t&) const;
+	point_t operator * (double) const;
+	point_t operator / (double) const;
+	point_t& operator += (const point_t&);
+	point_t& operator -= (const point_t&);
+	point_t operator - () const;
+
+	double givx() const { return x; }
+	double givey() const { return y; }
+	virtual double givez() const { return 0; }
+	virtual void set(const point_t& p) { x = p.x; y = p.y; }
+	void setx(double x) { point_t::x = x; }
+	void sety(double y) { point_t::y = y; }
+};
---
+++ polygon.h
@@ -0,0 +1,11 @@
+#ifndef POLYGON_T
+#define POLYGON_T
+
+class polygon_t {
+public:
+	polygon_t();
+	~polygon_t();
+private:
+	// polygon consists of a list of points.
+};
+#endif
